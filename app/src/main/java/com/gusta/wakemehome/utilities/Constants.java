f9ab90c90143a97de21ff918b3bf603df642e18f
omerlevy1248 <omerlevy1248g@gmail.com>
2019-09-30 21:43:05 +0300

    Add permission handling to GeofenceManager

---
+++ app/src/main/java/com/gusta/wakemehome/utilities/Constants.java
@@ -0,0 +1,9 @@
+package com.gusta.wakemehome.utilities;
+
+public final class Constants {
+
+    // TODO: Refactor use of constants
+    public static final String GEOFENCES_ADDED_KEY = "GEOFENCES_ADDED_KEY";
+    public static final int ACCESS_FINE_LOCATION_PERMISSION_REQUEST_CODE = 34;
+
+}
